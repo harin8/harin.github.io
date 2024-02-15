936639f4b61a0b13f7bcf81d07b10ed85312167d
Harin <32770966+harin8@users.noreply.github.com>
2024-02-15 13:07:12 -0500

    Update x.js

--- 2022/07/x.js
+++ 2022/07/x.js
@@ -1,2 +1,9 @@
 //alert(document.cookie);
 window.location.href = "http://www.w3schools.com";
+
+const nextURL = 'http://164.92.155.120/team';
+const nextTitle = 'Team';
+const nextState = { additionalInformation: 'Updated the URL with JS' };
+
+// This will create a new entry in the browser's history, without reloading
+window.history.pushState(nextState, nextTitle, nextURL);
